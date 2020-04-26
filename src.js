676c4a7578abdfccc7ad9ce60359ade9593d6bce
David <dvddevadze@gmail.com>
2020-04-26 04:53:41 +0400

    test3

---
+++ src.js
@@ -0,0 +1,4 @@
+const urlDefault  = 'https://api.giphy.com/v1/gifs/search?limit=10&api_key=aFFKTuSMjd6j0wwjpFCPXZipQbcnw3vB&fmt=json&q=';
+const urlTrending = 'https://api.giphy.com/v1/gifs/trending?limit=10&api_key=aFFKTuSMjd6j0wwjpFCPXZipQbcnw3vB&fmt=json';
+
+export{urlDefault,urlTrending};
